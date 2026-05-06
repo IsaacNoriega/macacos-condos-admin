4ec698754c807c36ced893a9cc15237ab1735814
unknown <isaac.noriega@iteso.mx>
2026-05-06 01:49:52 -0600

    config: update API_BASE_URL to use Azure Cloud Domain with HTTPS

--- frontend/src/app/core/api.constants.ts
+++ frontend/src/app/core/api.constants.ts
@@ -1,7 +1,7 @@
 import { environment } from '../../environments/environment';
 
 // Cambiamos la URL de Railway por la IP de tu Gateway de Azure
-export const API_BASE_URL = 'http://172.214.17.219/api'; 
+export const API_BASE_URL = 'https://macacos-api.eastus.cloudapp.azure.com/api'; 
 
 export const STORAGE_KEYS = {
   token: 'macacos_token',
